d3f9d7e09f9ccc3959567dc12697d2ff071e69a2
Stephan Meißner <atitsbest.dev@gmail.com>
2016-05-15 20:55:52 +0200

    + Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## -----------------
+## makeCacheMatrix  creates a combination of data and functions to 
+##                  encapsulate the caching of the inverse function.
+## cacheSolve   Uses the encapsulation of makeCacheMatrix to calculate
+##              the inverse of the matrix provided. If calculated once
+##              the result is cached and used for the next request.
 
 ## Write a short comment describing this function
+## ------------------
+## Uses a closure (environment + function) to encapsulate the original 
+## matrix and the inverse of the matrix. 
+## It returns a list of accessor modifier functions.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -17,7 +27,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## ------------------
+## Can work with the result of makeCacheMatrix and uses the functions
+## to check for an already calculated inverse of the matrix. 
+## If not yet calculated, it does so and saves the inverse to the
+## result of makeCacheMatrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinverse()
